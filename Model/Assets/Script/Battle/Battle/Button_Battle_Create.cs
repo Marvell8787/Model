ee791d7ff4c8ab4e9394e125221a2e1bfe11560c
Marvell8787 <46806099+Marvell8787@users.noreply.github.com>
2019-03-02 17:40:40 +0800

    20190302
    Add RoomWait Scene
    Add RoomWait Script
    Add RoomPlay Scene
    Add RoomPlay Script
    Add Settlement Scene
    Add Settlement Script

---
+++ Model/Assets/Script/Battle/Battle/Button_Battle_Create.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button_Battle_Create : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public void Click()
+    {
+        Debug.Log("Create");
+        Application.LoadLevel("RoomWait");
+    }
+}
